46502a56ba003415e32ac1bb9a6584d1171ac45d
mpetkov228 <mpetkov228@gmail.com>
2023-08-11 19:15:03 +0300

    Add gameboard cells programmatically instead of through html

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const container = document.querySelector('.container');
+
 const GameBoard = (function () {
     const gameBoardArr = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'O', 'X'];
 
-    
+    return {
+        gameBoardArr
+    };
 })();
+
+const board = GameBoard.gameBoardArr;
+
+for (let cell of board) {
+    const div = document.createElement('div');
+    div.textContent = cell;
+    container.appendChild(div);
+}
