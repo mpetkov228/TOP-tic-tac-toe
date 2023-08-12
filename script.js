653848522b3dfe8abbae7bc41cf78431457be4cc
mpetkov228 <mpetkov228@gmail.com>
2023-08-12 14:07:31 +0300

    Add ability to choose between players

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector('.container');
+const playerChoiceContainer = document.querySelector('.player-choice-container');
+
+container.addEventListener('click', (event) => {
+    if (event.target.className != 'cell') {
+        return;
+    }
+
+    event.target.textContent = currentPlayer.marker;
+});
+
+playerChoiceContainer.addEventListener('click', (event) => {
+    if (event.target.className == 'player-one') {
+        currentPlayer = Player.player1;
+        return;
+    }
+    if (event.target.className == 'player-two') {
+        currentPlayer = Player.player2;
+        return;
+    }
+});
 
 const GameBoard = (function () {
-    const gameBoardArr = ['', 'O', '', '', '', '', '', '', 'X'];
+    const gameBoardArr = ['', '', '', '', '', '', '', '', ''];
 
 
     const renderBoard = () => {
         for (let cell of gameBoardArr) {
             const div = document.createElement('div');
+            div.className = 'cell';
             div.textContent = cell;
             container.appendChild(div);
         }
@@ -31,6 +52,6 @@ const Player = (function () {
     };
 })();
 
-
+let currentPlayer = Player.player1;
 
 GameBoard.renderBoard();
