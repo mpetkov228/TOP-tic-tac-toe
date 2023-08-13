545880754ee00a20a3934e8257e0d7ef20b905c1
mpetkov228 <mpetkov228@gmail.com>
2023-08-13 13:10:18 +0300

    Move adding of click events to PlayGame module

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const container = document.querySelector('.container');
-const cells = document.querySelectorAll('.cell');
 const playerChoiceContainer = document.querySelector('.player-choice-container');
 
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        const index = cell.getAttribute('data-index');
-
-        GameBoard.gameBoardArr[index] = currentPlayer.marker;
-        cell.textContent = currentPlayer.marker;
-        
-        console.log(GameBoard.gameBoardArr);
-    }, { once: true });
-});
 
 playerChoiceContainer.addEventListener('click', (event) => {
     if (event.target.className == 'player-one') {
@@ -46,4 +35,26 @@ const Player = (function () {
     };
 })();
 
+const PlayGame = (function () {
+    const cells = document.querySelectorAll('.cell');
+
+    const addEvents = () => {
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                const index = cell.getAttribute('data-index');
+        
+                GameBoard.gameBoardArr[index] = currentPlayer.marker;
+                cell.textContent = currentPlayer.marker;
+                
+                console.log(GameBoard.gameBoardArr);
+            }, { once: true });
+        });
+    }
+    
+    return {
+        addEvents
+    };
+})();
+
 let currentPlayer = Player.player1;
+PlayGame.addEvents();
